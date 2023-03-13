1415e9fac1c9c3650ea5a526df840c16188ad404
Arangajanan Sivasubramaniam <47890434+Ari-arangajanan@users.noreply.github.com>
2023-03-13 14:39:20 +0530

    spring project upload

---
+++ src/test/java/com/example/UserHandlerApp/UserHandlerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserHandlerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserHandlerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
